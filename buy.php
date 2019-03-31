189c1fd0251f3ed9176e1a2542b5f0f270141c48
Drew <drewjacobson@live.com>
2019-03-31 18:11:56 -0400

    Add files via upload

---
+++ buy.php
@@ -0,0 +1,10 @@
+<html>
+	<div class="wrap_menu">
+		<form action="/action_page.php">
+			First name: <input type="text" name="fname"><br>
+			Last name: <input type="text" name="lname"><br>
+		<input type="submit" value="Submit">
+</form>
+	</div>
+
+</html>
---
+++ sell.php
@@ -0,0 +1,11 @@
+<html>
+	<div class="wrap_menu">
+		<form action="/action_page.php">
+			Item Name: <input type="text" name="name"><br>
+			Price: <input type="text" name="price"><br>
+			Price: <input type="text" name="price"><br>
+		<input type="submit" value="Submit">
+</form>
+	</div>
+
+</html>
