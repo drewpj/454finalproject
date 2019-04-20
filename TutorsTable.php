39c84cea93711333b40c15c30e1e40b88c12a44a
drewpj <drewjacobson@live.com>
2019-04-19 21:48:31 -0400

    m ch

--- TutorsTable.php
+++ TutorsTable.php
@@ -3,11 +3,11 @@
 			for($i = 0; $i < 5; $i++){
 				$sql = "sql statement";
 				echo "<tr>";
-				$id = "myCheck" + $i;
+				$id = "myCheck" . $i;
 				echo "<td> FakeMajor </td>";
 				echo "<td> $6.67 </td>";
 				echo "<td> A very good description </td>";
-				echo "<td> <input type=\"checkbox\" id= \" " + $id + " \" onclick=\"msg()\" value=1> </td>";
+				echo "<td> <input type=\"checkbox\" id= \" " . $id . " \" onclick=\"msg()\" value=1> </td>";
 				echo "</tr>";
 			}
 
