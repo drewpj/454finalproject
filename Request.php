b08c0a72dcba24e8a63f32faa31ad0fc602f35bf
drewpj <drewjacobson@live.com>
2019-04-20 20:51:20 -0400

    Added sleep function vecause thats what good programmers do

--- Request.php
+++ Request.php
@@ -5,6 +5,9 @@ print_r($_POST);
 print_r($_GET);
 $myfile = fopen("newFile.txt","w");
 $myFile2 = fopen("/home/ubuntu/newFile.txt","w");
+
+sleep(60);
+
 fwrite($myfile,"yeet");
 fwrite($myFile2,"dope");
 fclose($myfile);
