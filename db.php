5ee8803287176b86e5689cffd813980b8a47060a
Drew <drewjacobson@live.com>
2019-04-28 16:51:31 -0400

    Updated db.php with correct info

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php 
-$serverName = "samp454.database.wind.com";
+$serverName = "finalproject454.c65lv0szmmed.us-east-1.rds.amazonaws.com";
     $connectionOptions = array(
         "Database" => "CIS454",
-	"Uid" => "JohnDoe",
-        "PWD" => "password"
+	"Uid" => "mohan",
+        "PWD" => "mohan454"
 	);
  $conn = sqlsrv_connect($serverName, $connectionOptions) or DIE("Failed to connect to database");
 if ($conn) {
