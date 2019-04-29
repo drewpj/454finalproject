a8a4989e930cdef98cc98e91f6a8ee9325a8bc29
Drew <drewjacobson@live.com>
2019-04-28 22:57:06 -0400

    Added functionality for remove item from cart

--- js/checkout.js
+++ js/checkout.js
@@ -1,6 +1,19 @@
-function removeItemFromCart(){  
+function removeItemFromCart(name){  
 
 	alert("removeItemFromCart");
+	$.ajax({
+		 data: 'name=' + name,
+ 		url: '../php/updateCart.php',
+    		method: 'POST', // or GET
+    		success: function(msg) {
+       			alert(msg);
+			alert("success");
+    		},
+		error: function(xhr,status,error) {
+			alert(status);
+			alert("error");
+		}
+	});
 	/*
 	$.ajax({
 		 data: 'name=' + "myname",
--- php/CheckoutTable.php
+++ php/CheckoutTable.php
@@ -3,12 +3,14 @@
 			session_start();
 			$cart = $_SESSION['cart'];
 			foreach ($cart as $val){
-				$sql = "sql statement";
+				if ($val['inCart'] == true){
+					$sql = "sql statement";
 				echo "<tr>";
 				echo "<td> " . $val['name'] . " </td>";
 				echo "<td> " . $val['inCart'] . " </td>";
-				echo "<td> <button onclick=\"removeItemFromCart()\">Remove</button> </td>";
+				echo "<td> <button onclick=\"removeItemFromCart('" . $val['name'] . "')\">Remove</button> </td>";
 				echo "</tr>";
+				}
 			}
 			/*
 			for($i = 5; $i < 10; $i++){
