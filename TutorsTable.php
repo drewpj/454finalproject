d02a1494c6101d2bd6c51d2651305e636bfc2e09
Drew <drewjacobson@live.com>
2019-04-28 21:17:34 -0400

    Removed some escape characters

--- TutorsTable.php
+++ TutorsTable.php
@@ -8,7 +8,7 @@
 				echo "<td> $6.67 </td>";
 				echo "<td> A very good description </td>";
 				
-				echo "<td> <input type=\"checkbox\" id= \" " . $id . " \" onclick=\"msg(\\\"FakeMajor" . $i . "\\\")\" value=1> </td>";
+				echo "<td> <input type=\"checkbox\" id= \" " . $id . " \" onclick=\"msg(\'FakeMajor" . $i . "\')\" value=1> </td>";
 				echo "</tr>";
 			}
 
