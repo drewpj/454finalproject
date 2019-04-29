8666fcbd6a4b02d1b484b0708990bb86fe188e98
Drew <drewjacobson@live.com>
2019-04-28 21:13:36 -0400

    Added extra escape character

--- TutorsTable.php
+++ TutorsTable.php
@@ -7,7 +7,8 @@
 				echo "<td> FakeMajor" . $i . " </td>";
 				echo "<td> $6.67 </td>";
 				echo "<td> A very good description </td>";
-				echo "<td> <input type=\"checkbox\" id= \" " . $id . " \" onclick=\"msg(\"FakeMajor" . $i . "\")\" value=1> </td>";
+				
+				echo "<td> <input type=\"checkbox\" id= \" " . $id . " \" onclick=\"msg(\\"FakeMajor" . $i . "\\")\" value=1> </td>";
 				echo "</tr>";
 			}
 
