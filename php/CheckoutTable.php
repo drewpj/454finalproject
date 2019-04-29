0b1ede3b30135d9ef9e4a2abd2a4b29545447ab4
Drew <drewjacobson@live.com>
2019-04-28 22:29:15 -0400

    Added fun cart stuff

--- php/CheckoutTable.php
+++ php/CheckoutTable.php
@@ -1,7 +1,16 @@
 <?php
 
 			session_start();
-			
+			$cart = $_SESSION['cart'];
+			foreach ($cart as $val){
+				$sql = "sql statement";
+				echo "<tr>";
+				echo "<td> " . $val . " </td>";
+				echo "<td> $99 </td>";
+				echo "<td> <button onclick=\"removeItemFromCart()\">Remove</button> </td>";
+				echo "</tr>";
+			}
+			/*
 			for($i = 5; $i < 10; $i++){
 				$sql = "sql statement";
 				echo "<tr>";
@@ -10,5 +19,6 @@
 				echo "<td> <button onclick=\"removeItemFromCart()\">Remove</button> </td>";
 				echo "</tr>";
 			}
+			*/
 
 ?>
