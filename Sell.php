f1d163ec0f7842e283bd4458b9ca1334e1af1348
Kai Bernstein <kai.bernstein@gmail.com>
2019-04-30 12:51:51 -0400

    Update Sell.php

--- Sell.php
+++ Sell.php
@@ -216,14 +216,7 @@
 		}
 		
 		function add_item() { 
-			<?php
-				include_once 'include/db.php';
-	
-				$sql = "INSERT INTO Item (Name, ID, Recurrence, Price, Description, Category, Seller ) 
-				VALUES (title, '" . uniqid() . "', '14', price, brief, product, name)";
-				$result = sqlsrv_query($conn, $sql); 
-				echo "done";
-			?>
+			
 		}
 	</script>
 
