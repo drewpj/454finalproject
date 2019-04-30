f2bc15558897487f026bebb2bd1c4061f301b3f8
Drew <drewjacobson@live.com>
2019-04-30 15:30:01 -0400

    Login system created

---
+++ php/login.php
@@ -0,0 +1,35 @@
+<?php
+	include_once '../include/db.php';
+	session_start();
+	echo "\nLogin Running";
+
+	
+	
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	//SQL statement to be queried.
+	$sql = "SELECT Name, ID, Category FROM dbo.Users WHERE email='" . $email ."' AND PWDCOMPARE('" . $password . "', Password) = 1"; 
+	
+	$result = sqlsrv_query($conn, $sql);
+	
+		
+		
+	if (sqlsrv_has_rows($result)) {
+		echo("Logged in successfully!");
+		//Get row results
+		$row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
+		//Set user_type and user name variables.
+		$category = $row['Category'];
+		$_SESSION['userType'] = $category;
+		$_SESSION['ID'] = $row['ID'];
+		header("Location: ../Home.php?login=success");
+	//Otherwise, say password or username error.
+	} else {
+		//Error message if wrong password.
+		$message = "Bad Login info!";
+		echo("<script type='text/javascript'> alert('$message'); </script>");
+	}
+	
+	
+	echo "\nLogin ran";
+?>
