f6be90c71f903693e6f70a5eabde063bcd797266
Kai Bernstein <kai.bernstein@gmail.com>
2019-04-30 14:19:27 -0400

    Update AddToDB.php

--- AddToDB.php
+++ AddToDB.php
@@ -1,8 +1,14 @@
 <?php
 	include_once 'include/db.php';
 	
+	$name = $_POST['name'];
+	$price = $_POST['price'];
+	$brief = $_POST['brief'];
+	$product = $_POST['product'];
+	$title = $_POST['title'];
+
 	$sql = "INSERT INTO Item (Name, ID, Recurrence, Price, Description, Category, Seller ) 
-	VALUES (title, '" . uniqid() . "', '14', price, brief, product, name)";
+	VALUES ($title, '" . uniqid() . "', '14', $price, $brief, $product, $name)";
 	$result = sqlsrv_query($conn, $sql); 
 	
 	echo "done";
