e782700b000f216de40e9706292f518967179bf1
Drew <drewjacobson@live.com>
2019-04-30 01:06:20 -0400

    Update AddToDB.php

--- AddToDB.php
+++ AddToDB.php
@@ -1,10 +1,10 @@
 <?php
 	include_once 'include/db.php';
 	
-	$sql = "INSERT INTO Item (Name, ID, Recurrence, Price, Description, Category, Seller ) 
-	VALUES ('CIS321', '" . uniqid() . "', '14', 10.21, 'OK tutoring services', 'Tutor', 'Sheila'),
-			('A Great American Tour', '" . uniqid() . "', '0', 11.00, 'An OK book', 'Book', 'Guy Fieri');"
+	$sql = "INSERT INTO Item (Name, ID, Recurrence, Price, Description, Category, Seller ) VALUES ('CIS321', '" . uniqid() . "', '14', 10.21, 'OK tutoring services', 'Tutor', 'Sheila'),('A Great American Tour', '" . uniqid() . "', '0', 11.00, 'An OK book', 'Book', 'Guy Fieri');"
 	$result = sqlsrv_query($conn, $sql);
 	//$result = uniqid(); 
+	
+	echo "done";
 
 ?>
