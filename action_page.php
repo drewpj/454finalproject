4ce2b7b387dca0d991af7980fcf0d4d48363659d
Drew <drewjacobson@live.com>
2019-04-30 22:18:23 -0400

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -9,18 +9,18 @@
 	$title = $_POST['title'];
 	$author = $_POST['location'];
 	
-	if ($price > 0 && !empty($title)) {
+	//if ($price > 0 && !empty($title)) {
 		$id = uniqid();	
 		$sql = "INSERT INTO Item (Name, ID, Recurrence, Price, Description, Category, Seller, Author) 
 		VALUES ('". $title . "', '". $id ."', 0, '".$price."', '". $brief ."', '". $product ."', '". $name ."', '". $author ."');";
 		echo($sql);
 		$result = sqlsrv_query($conn, $sql); 
 		//header("Location: /SellAgain.php");
-	}
-	else {
+	//}
+	//else {
 		//$message = "Invalid input";
-		header("Location: /SellFail.php");
+		//header("Location: /SellFail.php");
 		//echo("<script type='text/javascript'> alert('$message'); </script>");
-	}
+	//}
 	
 ?>
