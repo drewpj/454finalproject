8f6d8482d05f0785bf6aedeb1750aab6481749f9
Kai Bernstein <kai.bernstein@gmail.com>
2019-04-30 20:40:40 -0400

    Update Sell.php

--- Sell.php
+++ Sell.php
@@ -54,7 +54,7 @@
 
 
 		<div class="container">
-		  <form action="/action_page.php" method="post">
+		  <form class="login" action="/action_page.php" method="post">
 
 		   <div class="row">
  		     <div class="col-25">
