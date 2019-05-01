a5e50605953844ee1c2631030c7928e8cd10e72d
Drew <drewjacobson@live.com>
2019-04-30 21:20:27 -0400

    Added electronics functionality

---
+++ php/ElectronicsTable.php
@@ -0,0 +1,18 @@
+<?php
+
+	include_once 'include/db.php';
+		
+	$sql = "SELECT * from Item WHERE category='Electronics'";
+	
+	$result = sqlsrv_query($conn, $sql);
+
+	while( $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC) ) {
+		echo "<tr>";
+		echo "<td>" . $row["Name"] . " </td>";
+		echo "<td> " . $row["Price"] . " </td>";
+		echo "<td> " . $row["Description"] . " </td>";
+		
+		echo "<td> <input type=\"checkbox\" id= \" " . $row["ID"] . " \" onclick=\"msg('" . $row["Name"] . "','". $row["ID"]."'," . $row["Price"] . ")\" value=1> </td>";
+		echo "</tr>";
+	}
+?>
