80d92c27204afd450b9ad6c2209a933df56f24a2
Kai Bernstein <kai.bernstein@gmail.com>
2019-04-30 23:20:41 -0400

    Update addUser.php

--- php/addUser.php
+++ php/addUser.php
@@ -10,11 +10,11 @@
 	$password2 = $_POST['password2'];
 	//$regex = "/.edu$/"
 	
-	//if(!preg_match($regex, $email))
-		//echo "bad email";
+	if(!preg_match($regex, $email))
+		echo "bad email";
 		
-	//if(!preg_match($regex, $email2))
-		//echo "bad email";
+	if(!preg_match($regex, $email2))
+		echo "bad email";
 
 	if (empty($name) || empty($familyname) || empty($email) || empty($email2) || empty($password) || empty($password2))
 		echo "invalid input";
