165603d77cfb5a15069b76ba05cada07944dad44
Kai Bernstein <kai.bernstein@gmail.com>
2019-04-30 21:25:35 -0400

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -9,7 +9,7 @@
 	$product = $_POST['product'];
 	$title = $_POST['title'];
 	
-	if ($price > 0 && $freq > 0 && $title != "") {
+	if ($price > 0 && $freq > 0 && !empty($_POST['title']) {
 		$sql = "INSERT INTO Item (Name, ID, Recurrence, Price, Description, Category, Seller ) 
 		VALUES ($title, '" . uniqid() . "', '$freq, $price, $brief, $product, $name)";
 		$result = sqlsrv_query($conn, $sql);
