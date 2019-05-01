2f723bae2f89cc147c15eb502c4d561b0b2cc168
Drew <drewjacobson@live.com>
2019-04-30 20:25:15 -0400

    Update TutorsTable.php

--- TutorsTable.php
+++ TutorsTable.php
@@ -18,7 +18,7 @@
 		echo "<td> " . $row["Price"] . " </td>";
 		echo "<td> " . $row["Description"] . " </td>";
 		
-		echo "<td> <input type=\"checkbox\" id= \" " . $row["id"] . " \" onclick=\"msg('" . $row["Name"] . "','". $row["ID"]."')\" value=1> </td>";
+		echo "<td> <input type=\"checkbox\" id= \" " . $row["id"] . " \" onclick=\"msg('" . $row["Name"] . "','". $row["ID"]."','".$row["Price"]."')\" value=1> </td>";
 		echo "</tr>";
 	}
 	//echo "\nend for loop";
