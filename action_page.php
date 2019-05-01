8bfd11cb6e20dda30e27fd7dc3cd5259699021b3
Kai Bernstein <kai.bernstein@gmail.com>
2019-04-30 20:54:03 -0400

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -12,12 +12,12 @@
 		$sql = "INSERT INTO Item (Name, ID, Recurrence, Price, Description, Category, Seller ) 
 		VALUES ($title, '" . uniqid() . "', '$freq, $price, $brief, $product, $name)";
 		$result = sqlsrv_query($conn, $sql);
-		header("Location: /Sell.php");
+		header("Location: /SellAgain.php");
 	}
 	else {
-		$message = "Invalid input";
-		header("Location: /Sell.php");
-		echo("<script type='text/javascript'> alert('$message'); </script>");
+		//$message = "Invalid input";
+		header("Location: /SellFail.php");
+		//echo("<script type='text/javascript'> alert('$message'); </script>");
 	}
 	
 ?>
