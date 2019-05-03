f935170b167d2c8460eb58b07a10ad496142b10a
Drew <drewjacobson@live.com>
2019-05-02 22:34:53 -0400

    Update submitPayment.php

--- php/submitPayment.php
+++ php/submitPayment.php
@@ -3,27 +3,27 @@
 	include_once '../include/db.php';
 	session_start();
 	$cart = $_SESSION['cart'];
-	$sum = 0;
+	//$sum = 0;
 	foreach ($cart as $val){
 		if ($val['inCart'] == true){
 			
-			$sum = $sum + $val['price'];
+			//$sum = $sum + $val['price'];
 
-			$sql = "SELECT * from Item WHERE ID='" . $val['id'] . "'";
-			echo $sql;
-			$result = sqlsrv_query($conn, $sql);
+			$sql1 = "SELECT * from Item WHERE ID='" . $val['id'] . "'";
+			echo $sql1;
+			$result = sqlsrv_query($conn, $sql1);
 		
 			$row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC);
-
+			echo "    |      row:".$row."          |    ";
 			$sellerID = $row["Seller"];
-			echo "    |      ".$sellerID."          |    ";
+			echo "    |      seller:".$sellerID."          |    ";
 			$buyerID = $_SESSION['ID'];
 			$time = time();
-			$sql = "INSERT INTO Transactions (Items, Seller, Buyer, Time) VALUES ('".$val['name']."','".$sellerID."','".$buyerID."',".$time.")";
-			echo $sql;
+			$sql2 = "INSERT INTO Transactions (Items, Seller, Buyer, Time) VALUES ('".$val['name']."','".$sellerID."','".$buyerID."',".$time.")";
+			echo $sql2;
 			//echo "\n\n" . $sql;
 		
-			$result = sqlsrv_query($conn, $sql); 
+			$result = sqlsrv_query($conn, $sql2); 
 		}
 	}
 	
