cba8014385ae212c4d8fe1b67c344da5172eddb6
Drew <drewjacobson@live.com>
2019-05-03 00:16:26 -0400

    Update submitPayment.php

--- php/submitPayment.php
+++ php/submitPayment.php
@@ -10,13 +10,14 @@
 			//$sum = $sum + $val['price'];
 
 			$sql1 = "SELECT * from Item WHERE ID='" . $val['id'] . "';";
-			echo $sql1;
+			//echo $sql1;
 			$result = sqlsrv_query($conn, $sql1);
 		
 			$row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC);
-			echo "    |      row:".$row."          |    ";
-			$sellerID = $row['Seller'];
-			echo "    |      seller:".$sellerID."          |    ";
+			//echo "    |      row:".$row."          |    ";
+			//$sellerID = $row['Seller'];
+			$sellerID = $val['seller'];
+			//echo "    |      seller:".$sellerID."          |    ";
 			$buyerID = $_SESSION['ID'];
 			$time = time();
 			$sql2 = "INSERT INTO Transactions (Items, Seller, Buyer, Time) VALUES ('".$val['name']."','".$sellerID."','".$buyerID."',".$time.")";
